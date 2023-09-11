b1cc27eb43ddbbc73b5222580b9506493f2ebcfd
saeedbaghestani <saeedbaghestani@outlook.com>
2023-09-11 19:07:34 +0330

    saeedbaghestani

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": { "item1":"value_Item1", "item2":"value_Item2"},"Isbn":"1" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {},"Isbn":"2" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {},"Isbn":"3" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} ,"Isbn":"4"},
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} ,"Isbn":"5"},
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {},"Isbn":"6" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {},"Isbn":"7" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} ,"Isbn":"8"},
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} ,"Isbn":"9"},
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} ,"Isbn":"10"}
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,115 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	const username = req.body.username
+	const password = req.body.password
+
+	if (username && password) {
+		if (!isValid(username)) {
+			users.push({username: username, password: password})
+			return res.status(200).json({
+				message: 'User successfully registered. Now you can login',
+			})
+		} else {
+			return res.status(404).json({message: 'User already exists!'})
+		}
+	}
+	return res.status(404).json({message: 'Unable to register user.'})
+});
+
+public_users.get('/',function (req, res) {
+ 
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("asyncbooks", async function (req,res) {
+  try {
+    let response = await axios.get("http://localhost:5000/");
+    console.log(response.data);
+    return res.status(200).json(response.data);
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({message: "Error getting book list"});
+  }
+});
+
+public_users.get("/asyncbooks/author/:author", function (req,res) {
+  let {author} = req.params;
+  axios.get(`http://localhost:5000/author/${author}`)
+  .then(function(response){
+    console.log(response.data);
+    return res.status(200).json(response.data);
+  })
+  .catch(function(error){
+      console.log(error);
+      return res.status(500).json({message: "Error while fetching book details."})
+  })
+});
+
+public_users.get("/asyncbooks/title/:title", function (req,res) {
+  let {title} = req.params;
+  axios.get(`http://localhost:5000/title/${title}`)
+  .then(function(response){
+    console.log(response.data);
+    return res.status(200).json(response.data);
+  })
+  .catch(function(error){
+      console.log(error);
+      return res.status(500).json({message: "Error while fetching book details."})
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+    if (books[isbn]) {
+        return res.json(books[isbn]);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+   });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;  
+  const authorBooks = [];  
+  
+  for (const book in books) {  
+    if (books[book].author === author) {  
+      authorBooks.push(books[book]);
+    }
+  }
+  
+  if (authorBooks.length > 0) {  
+    res.send(authorBooks);  
+  } else {
+    res.status(404).send('No books found with that author');  
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase().includes(title));
+  if(filteredBooks.length > 0){
+      return res.status(200).json(filteredBooks);
+  }
+  else{
+      return res.status(404).json({message: "Book not found"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	const reviewISBN = req.params.isbn
+
+	Object.entries(books).map(book => {
+		if (book[0] === reviewISBN) {
+			res.send(book[1].reviews)
+		}
+	})
 });
 
 module.exports.general = public_users;
